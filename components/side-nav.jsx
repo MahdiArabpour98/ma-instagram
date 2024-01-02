73c1affd30de19029040b4bd869146de8b52dda4
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-01-02 09:59:12 +0330

    add base layout

---
+++ components/side-nav.jsx
@@ -0,0 +1,17 @@
+import Logo from "./logo";
+import { Button } from "./ui/button";
+
+const SideNav = () => {
+  return (
+    <div>
+      <div>
+        <Logo />
+        {/* <NavLinks/> */}
+        {/* <ProfileLink/> */}
+        <div>{/* <MoreDropdown/> */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
