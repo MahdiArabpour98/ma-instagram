5bef0ad4a74125798d3bb0a0a9508e0428c09274
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-01-03 13:50:59 +0330

    add nav links

--- components/logo.jsx
+++ components/logo.jsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { SwitchCamera } from "lucide-react";
+
 const Logo = () => {
-  return <div>logo</div>;
+  return (
+    <Link
+      href={"/dashboard"}
+      className={buttonVariants({
+        className: "hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0",
+        variant: "ghost",
+        size: "lg",
+      })}
+    >
+      <SwitchCamera className="h-6 w-6 shrink-0 lg:hidden" />
+      <p className="font-semibold text-xl hidden lg:block">Instagram</p>
+    </Link>
+  );
 };
 
 export default Logo;
