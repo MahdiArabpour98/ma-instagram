4366abf5b3b69b5ce36b257a54173122052c2b1e
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-01-07 10:53:53 +0330

    get and delete posts

--- components/posts.jsx
+++ components/posts.jsx
@@ -1,14 +1,15 @@
-// import { fetchPosts } from "@/lib/data";
-// import Post from "./post";
+import { fetchPosts } from "@/lib/data";
+import Post from "./post";
 
 async function Posts() {
-  // const posts = await fetchPosts();
+  const posts = await fetchPosts();
+  console.log("posts", posts);
 
   return (
     <>
-      {/* {posts.map((post) => (
+      {posts.map((post) => (
         <Post key={post.id} post={post} />
-      ))} */}
+      ))}
     </>
   );
 }
